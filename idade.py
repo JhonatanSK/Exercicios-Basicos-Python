22d6aedc310349790bcc29a0f4fc247889a13055
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2019-06-30 23:03:57 -0300

    Programa que define se é maior ou menor de idade

---
+++ idade.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+idade = int(input("Digite sua idade: "))
+
+if idade >= 0 and idade < 18:
+    print("Menor de idade!")
+elif idade >= 18:
+    print("Maior de idade!")    
