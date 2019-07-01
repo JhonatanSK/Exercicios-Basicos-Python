2f45a5089413da1eb82a4191eebe1558eb84ac29
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2019-06-30 23:16:41 -0300

    Programa que calcula média de notas

---
+++ mediaNova.py
@@ -0,0 +1,8 @@
+valor1 = int(input("Digite a primeira nota: "))
+valor2 = int(input("Digite a segunda nota: "))
+resultado = (valor1 + valor2)/2
+
+if resultado >= 6:
+    print("Aprovado!")
+elif resultado <6:
+    print("Reprovado!")        
